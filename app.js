886cea1b2bc333ab153db40aeb5c7842a96eaf69
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-10-05 14:09:03 +0530

    update

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require('express')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 const user = require('./routes/user')
 const car = require('./routes/car')
 
 const app = express()
-const port = 4000
+const port = 8000
+
 app.use(express.json())
 
 app.use('/user',user)
 app.use('/car',car)
 
-const url = 'mongodb://127.0.0.1/reactbackend'
+// const url = 'mongodb://127.0.0.1/carSaleBackend'
 
-mongoose.connect(url,{useNewUrlParser: true})
-const con = mongoose.connection
+// mongoose.connect(url,{useNewUrlParser: true})
+// const con = mongoose.connection
 
-con.on("open",()=>{
-    console.log("MongoDB connected!");
-})
+// con.on("open",()=>{
+//     console.log("MongoDB connected!");
+// })
 
 app.listen(port, ()=>{
     console.log(`app listening port ${port}`);
--- routes/user.js
+++ routes/user.js
@@ -1,74 +1,101 @@
-const express = require('express')
-const app = express()
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+const db = require('../configs/db.configs');
+const { query } = require('express');
 
-app.use(express.json())
+const connection = mysql.createConnection(db.database);
 
-const User = require('../Models/user.models')
-
-router.get('/', async(req,res)=>{
-    console.log("get");
-    try {
-        const users = await User.find()
-        res.send(users)
-    } catch (error) {
-        res.send(error)
+connection.connect(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('connected to the MySQL Server');
+        var userTableQuery = "CREATE TABLE IF NOT EXISTS users (fullName VARCHAR(255),email VARCHAR(255),phoneNumber VARCHAR(255), username VARCHAR(255) PRIMARY KEY,password VARCHAR(255))";
+        connection.query(userTableQuery, function (err, result) {
+            if (result.warningCount === 0) {
+                console.log("User table created");
+            }
+        })
     }
 })
 
-router.post('/', (req,res)=>{
-    const data = req.body.formData
-    //console.log(data);
-    const newUser = new User({
-        fullName: data.fullName,
-        email: data.email,
-        phoneNumber: data.phoneNumber,
-        username: data.username,
-        password: data.password
-    });
-    try {
-        const response = newUser.save()
-        res.send("Saved!")
-    } catch (error) {
-        res.send(error)
-    }
+
+router.get('/login/:username/:password', (req, res) => {
+    const username = req.params.username
+    const password = req.params.password;
+
+    var query = "SELECT * FROM users WHERE username=? AND password=?";
+
+    connection.query(query, [username,password], (err, row) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(row);
+        }
+    })
 })
 
-router.put('/:id', async(req,res)=>{
-    const data = req.body
+router.post('/', (req, res) => {
+    const fullName = req.body.fullName;
+    const email=req.body.email;
+    const phoneNumber = req.body.phoneNumber;
+    const username = req.body.username;
+    const password = req.body.password;
 
-    try {
-        const user = await User.findById(req.params.id)
-        user.fullName = data.fullName,
-        user.email = data.email,
-        user.phoneNumber = data.phoneNumber,
-        user.username = data.username,
-        user.password = data.password
-        const response = await user.save()
+    var query = "INSERT INTO users (fullName,email,phoneNumber,username,password) VALUES (?,?,?,?)";
 
-        res.send("Updated!")
-    } catch (error) {
-        res.send(error)
-    }
-})
+    connection.query(query, [fullName,email, phoneNumber, username,password], (err) => {
+        if (err) {
+            res.send({
+                "status": "500",
+                "message": "Username Already Exists!"
+            });
+        } else {
+            res.send({
+                "status": "200",
+                "message": "User saved successfully"
+            });
+        }
+    })
 
-router.delete('/:id', async(req,res)=>{
-    try {
-        const user = await User.findById(req.params.id)
-        const response = user.remove()
-        res.send("Deleted!")
-    } catch (error) {
-        res.send(error)
-    }
-})
+});
 
-router.get('/:id', async(req,res)=>{
-    try {
-        const user = await User.findById(req.params.id)
-        res.send(user)
-    } catch (error) {
-        res.send(error)
-    }
-})
+// router.put('/:id', async(req,res)=>{
+//     const data = req.body
+
+//     try {
+//         const user = await User.findById(req.params.id)
+//         user.fullName = data.fullName,
+//         user.email = data.email,
+//         user.phoneNumber = data.phoneNumber,
+//         user.username = data.username,
+//         user.password = data.password
+//         const response = await user.save()
+
+//         res.send("Updated!")
+//     } catch (error) {
+//         res.send(error)
+//     }
+// })
+
+// router.delete('/:id', async(req,res)=>{
+//     try {
+//         const user = await User.findById(req.params.id)
+//         const response = user.remove()
+//         res.send("Deleted!")
+//     } catch (error) {
+//         res.send(error)
+//     }
+// })
+
+// router.get('/:id', async(req,res)=>{
+//     try {
+//         const user = await User.findById(req.params.id)
+//         res.send(user)
+//     } catch (error) {
+//         res.send(error)
+//     }
+// })
 
 module.exports = router
